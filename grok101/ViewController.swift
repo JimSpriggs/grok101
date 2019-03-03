115f843a286eeadb86cdd903de3f190b361164c7
John Hurst <jhurst1970@gmail.com>
2019-03-03 17:22:20 +0000

    p018 wip - calling jsonplaceholder

--- grok101/ViewController.swift
+++ grok101/ViewController.swift
@@ -13,6 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let todoEndpoint = "https://jsonplaceholder.typicode.com/todos/1"
+        guard let url = URL(string: todoEndpoint) else {
+            print("Error: cannot create URL")
+            return
+        }
+        let urlRequest = URLRequest(url: url)
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: urlRequest) { data, response, error in
+            if let response = response {
+                print(response)
+            }
+            if let error = error {
+                print(error)
+            }
+        }
+        task.resume()
+        
     }
 
 
