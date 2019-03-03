36d2c43b05042df7f3b95aa2b09482ec305a5c26
John Hurst <jhurst1970@gmail.com>
2019-03-03 16:50:13 +0000

    Initial Commit

---
+++ grok101/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  grok101
+//
+//  Created by John Hurst on 03/03/2019.
+//  Copyright © 2019 John Hurst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
